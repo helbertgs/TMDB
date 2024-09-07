16753e9569334a1ab1f267eda6c96e0510c368c7
Helbert Gomes <helbertgs@gmail.com>
2024-09-07 12:46:16 -0400

    feat: create xcodeproject

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Helbert Gomes on 2024-09-07.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
